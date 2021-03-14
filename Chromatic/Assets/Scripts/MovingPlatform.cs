29d3d7dbf79320757fb528224571d38ea57a12c6
bloo7883 <34141279+bloo7883@users.noreply.github.com>
2021-03-14 15:08:38 -0400

    MovingPlatform

---
+++ Chromatic/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public bool changeDir; //Changes the direction that the platform is moving
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeDir = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The platform can move left
+        if (changeDir == true)
+        {
+            transform.Translate(-Time.deltaTime * speed, 0, 0, Space.World);
+        }
+
+        //The platform can move right
+        else if (changeDir == false)
+        {
+            transform.Translate(Time.deltaTime * speed, 0, 0, Space.World);
+        }
+    }
+
+    //Changes the platform's direction of movement when it triggers any of the two invisible triggers
+    void OnTriggerEnter(Collider change)
+    {
+        if (change.gameObject.tag == "Moving")
+        {
+            //Changes platform's direction to the right
+            if (changeDir == true)
+            {
+                changeDir = false;
+            }
+            
+            //Changes platform's direction to the left
+            else if (changeDir == false)
+            {
+                changeDir = true;
+            }
+        }
+    }
+}
