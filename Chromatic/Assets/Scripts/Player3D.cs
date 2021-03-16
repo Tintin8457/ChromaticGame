e5cee9ef0c42558f7e2ff14bedf904f08cbed4ce
TriOmega <ericdanielosorio@gmail.com>
2021-03-16 00:09:33 -0400

    ShaderGraph, Jump Fix, Model Folder
        >Added ShaderGraph for future use
        >Changed Player3D script to create a consistent jump
            >All platforms are now a part of layer Ground
        >Removed old code from Player3D script
        >Added Model folder for artist's use

--- Chromatic/Assets/Scripts/Player3D.cs
+++ Chromatic/Assets/Scripts/Player3D.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Player3D : MonoBehaviour
 {
+    //Fixed the jumping and cleaned up by removing old code.
     
     private float horzMovement;
     private float vertMovement;
     private Rigidbody playerRB;
-    public float playerSpeed = 3.0f;
+    private SphereCollider playerCol;
+    public float playerSpeed = 3.0f; //Change speed in inspector
+    public float jumpForce = 10.0f; //Change jump force in inspector
+    public LayerMask groundLayer;
 
     private Renderer originalColor;
     public Material currentColor;
-    
-    public bool canJump = true;
-
-
-    //Change jump amount in inspector
-    public float jumpAmount = 1.0f;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerCol = GetComponent<SphereCollider>();
         originalColor = GetComponent<Renderer>();
         currentColor.color = this.originalColor.material.color;
     }
@@ -35,59 +34,28 @@ public class Player3D : MonoBehaviour
         this.originalColor.material.color = currentColor.color;
 
         //Press the spacebar to jump if the player can jump
-        if (canJump == true)
+        if(IsGrounded() && (Input.GetKeyDown(KeyCode.Space)))
         {
-            if((Input.GetKey(KeyCode.Space)))
-            {
-                playerRB.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
-            }
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log(Vector3.up * jumpForce);
         }
     }
 
     void FixedUpdate()
     {
-        //Old Player movement
-        //playerRB.AddForce(new Vector3(horzMovement * playerSpeed, vertMovement * playerSpeed));
-        //Vector3 movePlayer = new Vector3(horzMovement, 0, vertMovement);
-        //transform.position = new Vector3(horzMovement, 0 , vertMovement) * Time.deltaTime * playerSpeed;
-
-        //New player movement
+        //Player movement
         transform.Translate(new Vector3(horzMovement, 0 , 0) * Time.deltaTime * playerSpeed);
-    }
-
-    //////Jump//////
-    void OnCollisionStay(Collision collision)
-    {
-        //Old Jump code
-        // if (collision.gameObject.tag == "Ground" && isOnGround)
-        // {
-        //     if((Input.GetKey(KeyCode.W)) || (Input.GetKey("up")))
-        //     {
-        //         //playerRB.AddForce(new Vector2(0, 3), ForceMode.Impulse);
-        //         playerRB.AddForce(Vector3.up * 3.0f, ForceMode.Impulse);
-        //         //this.spriteRend.color = currentColor;
-        //     }
-        // }
-
-        //Player can only jump if they are ON the ground
-        if (collision.collider.tag == "Ground")
-        {
-            canJump = true;
-        }
-    }
-
-    //Player can NOT jump if they are NOT the ground
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.tag == "Ground")
-        {
-            canJump = false;
-        }
     }
 
     //Changing the player's color
     public void ChangeColor(Material paintColor)
     {
         currentColor.color = paintColor.color;
+    }
+
+    //Ground Check Function
+    public bool IsGrounded()
+    {
+        return Physics.CheckCapsule(playerCol.bounds.center, new Vector3(playerCol.bounds.center.x, playerCol.bounds.min.y, playerCol.bounds.center.z), playerCol.radius * 0.9f, groundLayer);
     }
 }
