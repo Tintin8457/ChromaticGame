b2dd1df1f3de2bb2c2b7282623a3d1a56a778ef7
bloo7883 <34141279+bloo7883@users.noreply.github.com>
2021-03-28 17:58:50 -0400

    BreakableWalls_Adjustments
    -the walls can now only be destroyed from a grayscale projectile
    -changed the color of the grayscale paint pickup to be light gray

---
+++ Chromatic/Assets/Scripts/BreakableWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour
+{
+    //Destroy breakable walls from a grayscale projectile
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Grayscale")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
