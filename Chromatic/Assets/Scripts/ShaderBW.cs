ffbc7dacf448173ccd8b54c66cbc7fdef0c93003
bloo7883 <34141279+bloo7883@users.noreply.github.com>
2021-03-31 01:33:41 -0400

    BW_Shader
    -Implemented a grayscale shader that desaturates an object from its material color(s).
    -I am using one of the building model assets as an example to demonstrate the bw to color functionality
    -the object is bw at start and becomes colorized again from the player shooting a projectile at it (MUST pick up a paintable pickup first before doing it)
    -the object is destroyed shortly after shooting, it is just for the small demo
    -by default, when you see mostly pink, except for the small building, the pipeline asset for the shader is used. Take it out by going to:
    edit- project settings- graphics- select the box under scriptable render pipeline settings and choose none
    -during my process in the prototyping scene, i messed up the lighting in there. (This does NOT affect the vertical slice scene.)

---
+++ Chromatic/Assets/Scripts/ShaderBW.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Only works with the pipeline asset applied in the graphics portion in project settings
+public class ShaderBW : MonoBehaviour
+{
+    public Material[] bwShader; //Attch all necessary materials for the shader and change the size in inspector
+    public bool canDestroy; //This is just for demo
+
+    //public MeshRenderer shaderBw;
+    //public Shader bw;
+    //public Vector4 satInput;
+    //public float saturation; //Change the saturation in the game
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bwShader.SetVector("RangeSaturation", new Vector4(1f, 0f, 0f, 0f));
+        //Debug.Log(bw.GetPropertyName(1));
+        //bwShader.SetFloat("_Saturation", 1f);
+        //shaderBw.material.SetFloat("_Saturation", 1f);
+        
+        //This object will start off as a black & white object
+        foreach (var mat in bwShader)
+        {
+            mat.SetFloat("_Saturation", 0f);
+        }
+
+        canDestroy = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //bwShader.GetVector("_Saturation");
+
+        //Destroy for demo only
+        if (canDestroy == true)
+        {
+            StartCoroutine("WaitToDestroy");
+        }
+    }
+
+    //The object will turn the object back into its colored form
+    void OnCollisionEnter(Collision colorize)
+    {
+        if (colorize.gameObject.tag == "Red" || colorize.gameObject.tag == "Yellow" || colorize.gameObject.tag == "Blue" || colorize.gameObject.tag == "Grayscale")
+        {
+            foreach (var mat in bwShader)
+            {
+                mat.SetFloat("_Saturation", 1f);
+            }
+
+            canDestroy = true;
+        }
+    }
+
+    IEnumerator WaitToDestroy()
+    {
+        yield return new WaitForSeconds(7f);
+        Destroy(gameObject);
+    }
+}
