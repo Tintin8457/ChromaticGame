cddcfa7408fb2e9c06e918ad93637d375ea2d7f0
bloo7883 <34141279+bloo7883@users.noreply.github.com>
2021-04-04 23:49:08 -0400

    MinorChanges
    -The yellow sticky platforms are no longer semi-transparent after recently looking at the GDD
    -Made the sticky floor into a prefab
    -Made some minor organization changes in shaderbw script and hierarchy

--- Chromatic/Assets/Scripts/ShaderBW.cs
+++ Chromatic/Assets/Scripts/ShaderBW.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 public class ShaderBW : MonoBehaviour
 {
     //Attch all necessary materials for the shader and change the size in inspector
+    [Header("Object Mesh Renderers")]
     public MeshRenderer[] bwShader; //This will be needed for all objects, included ones that use more than one mesh renderers
+
+    [Header("Shader Behavior")]
     public bool paintable; //Change its value in inspector to indicate what can be semi-transparent and desaturated
     public bool canBePainted; //Change shader look through platforms
+
+    [Header("Sticky Platforms")]
+    public bool stickyWall; //Prevents the sticky wall from being semi-transparent
+    public bool paintSticky; //Can only paint sticky platforms
+    
     private MaterialPropertyBlock bw; //Use to reference the bw shader graph properties
     MeshRenderer components; //Contains all colored elements of the object
 
@@ -35,6 +43,14 @@ public class ShaderBW : MonoBehaviour
                 components.SetPropertyBlock(bw);
                 canBePainted = true;
             }
+
+            //Make the sticky wall paintable and visible
+            else if (stickyWall == true)
+            {
+                bw.SetFloat("_Opacity", 1f);
+                components.SetPropertyBlock(bw);
+                canBePainted = true;
+            }
         }
         
         //For objects that use more than one mesh renderer
@@ -54,9 +70,16 @@ public class ShaderBW : MonoBehaviour
                     mat.SetPropertyBlock(bw);
                     canBePainted = true;
                 }
+
+                //Make the sticky wall paintable and visible
+                else if (stickyWall == true)
+                {
+                    bw.SetFloat("_Opacity", 1f);
+                    mat.SetPropertyBlock(bw);
+                    canBePainted = true;
+                }  
             }
         }
-        
     }
 
     // Update is called once per frame
@@ -102,6 +125,49 @@ public class ShaderBW : MonoBehaviour
                     else if (canBePainted == false)
                     {
                         bw.SetFloat("_Opacity", 1f);
+                        bw.SetFloat("_Saturation", 1f);
+                        mat.SetPropertyBlock(bw);
+                    }
+                }
+            }
+        }
+
+        //Change the sticky platform's saturation
+        else if (paintSticky == true)
+        {
+            //For objects that use one mesh renderer
+            if (components != null)
+            {
+                //Make desaturated to indicate that it needs to be painted
+                if (canBePainted == true)
+                {
+                    bw.SetFloat("_Saturation", 0f);
+                    components.SetPropertyBlock(bw);
+                }
+
+                //Make olorized to indicate that it has been painted
+                else if (canBePainted == false)
+                {
+                    bw.SetFloat("_Saturation", 1f);
+                    components.SetPropertyBlock(bw);
+                }
+            }
+
+            //For objects that use more than one mesh renderer
+            else if (components == null)
+            {
+                foreach (var mat in bwShader)
+                {
+                    //Make desaturated to indicate that it needs to be painted
+                    if (canBePainted == true)
+                    {
+                        bw.SetFloat("_Saturation", 0f);
+                        mat.SetPropertyBlock(bw);
+                    }
+
+                    //Make visible and colorized to indicate that it has been painted
+                    else if (canBePainted == false)
+                    {
                         bw.SetFloat("_Saturation", 1f);
                         mat.SetPropertyBlock(bw);
                     }
