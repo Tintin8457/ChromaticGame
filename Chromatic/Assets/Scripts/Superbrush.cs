115ce9e05862673e519a6f9c12f630af5bbc9290
TriOmega <ericdanielosorio@gmail.com>
2021-04-19 20:04:11 -0400

    Superbrush camera cweep & end screen text
    -Filled in some spots in the environment with assets so the camera sweep keeps the illusion of a floor without a void
    -Added WinText object for the victory end screen
    -Adjusted SLIMEBOX font material to have an outline for clearer visibility
    -Added a second camera with a sweeping animation that is switched to when the Superbrush is activated
    -Superbrush disables player UI elements and activates WinText, Play Again, and Quit.

--- Chromatic/Assets/Scripts/Superbrush.cs
+++ Chromatic/Assets/Scripts/Superbrush.cs
@@ -6,18 +6,53 @@ public class Superbrush : MonoBehaviour
 {
     public bool superBrushActive = false;
     public int requiredBristleAmount = 11;
+    public Camera mainCamera;
+    public Camera sweepCamera;
+    private Animator camSweepAnim;
+    public GameObject checkpointUI;
+    public GameObject curColorUI;
+    public GameObject nextColorUI;
+    public GameObject prevColorUI;
+    public GameObject bristleUI;
+    public GameObject timerUI;
+    public GameObject pauseUI;
+    public GameObject winUI;
+    public GameObject restartButton;
+    public GameObject quitButton;
     //Animation sb; //Show repair animation
     //public Camera cameraSweep; //Change camera that shows the entire level in full color horizontally back
 
+    void Start()
+    {
+        camSweepAnim = sweepCamera.GetComponent<Animator>();
+    }
     void OnTriggerEnter(Collider other)
     {
         Player3D player = other.GetComponent<Player3D>();
+        ShootingController shoot = other.GetComponent<ShootingController>();
         if (player != null)
         {
             if (player.bristles == requiredBristleAmount)
             {
                 {
                     superBrushActive = true;
+                    mainCamera.enabled = false;
+                    sweepCamera.enabled = true;
+                    camSweepAnim.enabled = true;
+                    checkpointUI.SetActive(false);
+                    curColorUI.SetActive(false);
+                    prevColorUI.SetActive(false);
+                    nextColorUI.SetActive(false);
+                    bristleUI.SetActive(false);
+                    timerUI.SetActive(false);
+                    pauseUI.SetActive(false);
+                    winUI.SetActive(true);
+                    restartButton.SetActive(true);
+                    quitButton.SetActive(true);
+                    player.playerSpeed = 0f;
+                    player.stopJumping = true;
+                    shoot.stopShooting = true;
+                    player.inkySound.Stop();
                 }
             }
         }
